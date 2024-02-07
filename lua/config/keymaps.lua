544927bf3bdf849337ecb28d21fd9eff6e2d557c
Miftahul Anwar <miftahul.anwar@zeniuseducation.com>
2024-02-07 19:19:36 +0700

    add some custom keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,19 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local keymap = vim.keymap
+keymap.set("i", "jk", "<ESC>")
+
+-- telescope
+keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>")
+keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>")
+keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>")
+keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>")
+keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<cr>")
+
+-- split pane
+keymap.set("n", "<leader>sv", "<C-w>v")
+keymap.set("n", "<leader>sz", "<C-w>s")
+keymap.set("n", "<leader>se", "<C-w>=")
+keymap.set("n", "<leader>sx", ":close<CR>")
